2348abece823b2a07e95719b864960004e98bac6
QUDUSKUNLE <quduskunle@gmail.com>
2022-08-11 15:46:20 +0100

    Write test for Add func

--- home_test.go
+++ home_test.go
@@ -3,9 +3,27 @@ package main
 import "testing"
 
 func TestHome(t *testing.T) {
+
+	assert_Hello_func := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	assert_Add_func := func(t *testing.T, got, want int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
+
 	t.Run("Hey, say Hello to Qudus Yekeen", func(t *testing.T) {
 		got := Hello("World", "Yoruba")
 		want := "Hello, World"
+
+		assert_Hello_func(t, got, want)
+
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
@@ -15,6 +33,8 @@ func TestHome(t *testing.T) {
 		got := Hello("Chris", "Yoruba")
 		want := "Hello, Chris"
 
+		assert_Hello_func(t, got, want)
+	
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
@@ -24,36 +44,43 @@ func TestHome(t *testing.T) {
 		got := Hello("", "Yoruba")
 		want := "Hello, World"
 
+		assert_Hello_func(t, got, want)
+
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
 
-	assertCorrectMessage := func(t *testing.T, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	}
-
 	t.Run("saying Hello to people in a loop", func(t *testing.T) {
 		got := Hello("Chris", "Yoruba")
 		want := "Hello, Chris"
-		assertCorrectMessage(t, got, want)
+
+		assert_Hello_func(t, got, want)
+
 	})
 
 	t.Run("empty string defaults to 'World'", func(t *testing.T) {
 		got := Hello("", "Yoruba")
 		want := "Hello, World"
-		assertCorrectMessage(t, got, want)
+		assert_Hello_func(t, got, want)
 	})
 
 	t.Run("testing adder function", func(t *testing.T) {
 		sum := Add(2, 4)
 		expected := 6
-
+		assert_Add_func(t, sum, expected)
 		if sum != expected {
 			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
+
+	t.Run("testing adder function", func(t*testing.T) {
+		summ := Add(2, 9)
+		expected := 11
+		assert_Add_func(t, summ, expected)
+		if summ != expected {
+			t.Fail()
+			t.Errorf("expected '%d' but got '%d'", expected, summ)
 		}
 	})
 }
