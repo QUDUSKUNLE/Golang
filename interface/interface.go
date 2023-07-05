4f7748bea1a4eb46b21604016bd806bd3a2fb43b
QUDUSKUNLE <quduskunle@gmail.com>
2023-07-05 16:04:09 +0100

    interfaces defintition

--- interface/interface.go
+++ interface/interface.go
@@ -20,3 +20,65 @@ func DisPlayType(interf interface{}) interface{} {
 		return "unknown"
 	}
 }
+
+type Employee interface {
+	PrintName(name string)
+	PrintSalary(basic, tax  int) int
+}
+
+type Polygons interface {
+	Perimeter()
+}
+
+type Object interface {
+	NumberOfSide()
+}
+
+type Vehicle interface {
+	Structure() []string
+	Speed() string
+}
+
+type Human interface {
+	Structure() []string
+	Performance() string
+}
+
+// Emp user-defined type
+type Emp int
+type Pentagon int
+type Car string
+type Man string
+
+func (e Emp) PrintName(name string) {
+	fmt.Println("Employee Id:\t\t", e)
+	fmt.Println("Employee Name:\t\t", name)
+}
+
+func (e Emp) PrintSalary(basic, tax int) int {
+	return basic -  ((basic * tax) / 100)
+}
+
+func (p Pentagon) Perimeter() {
+	fmt.Println("Perimeter of Pentagon:\t", 5*p)
+}
+
+func (p Pentagon) NumberOfSide() {
+	fmt.Printf("Pentagon has %v sides", p)
+}
+
+func (c Car) Structure() []string {
+	return []string{"ECU", "Engine", "Air Filters", "Wipers", "Gas Tank"}
+}
+
+func (c Car) Speed() string {
+	return "200 Km/Hrs"
+}
+
+func (m Man) Structure() []string {
+	return []string{"Brain", "Heart", "Nose", "Eyelashes", "Stomach"}
+}
+
+func (m Man) Performance() string {
+	return "8 Hrs/Day"
+}
--- main.go
+++ main.go
@@ -12,4 +12,13 @@ func main() {
 	pointer.Increment(&age)
 	fmt.Println(age)
 	fmt.Println(interfaces.DisPlayType(float64(12)))
+
+	var employee interfaces.Employee = interfaces.Emp(2)
+	employee.PrintName("Abdul-Quddus Yekeen")
+	fmt.Println("Employee Salary:\t", employee.PrintSalary(25000, 5))
+
+	var polygon interfaces.Polygons = interfaces.Pentagon(5)
+	polygon.Perimeter()
+	var pentagon interfaces.Pentagon = polygon.(interfaces.Pentagon)
+	pentagon.NumberOfSide()
 }
