b70a7e2ba8943b40dd12deb9b565d27ef2309d70
QUDUSKUNLE <quduskunle@gmail.com>
2024-06-23 11:09:24 +0100

    Add Middlewares

--- server/goFiber/server.go
+++ server/goFiber/server.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 import "gofiber/models"
+import "gofiber/middlewares"
 
 func main() {
 	app := fiber.New()
@@ -9,5 +10,6 @@ func main() {
 	app.Get("/", models.GetHome)
 	app.Get("/:name?", models.GetName)
 	app.Get("/api/*", models.GetAPI)
+	app.Use(middlewares.Middleware)
 	app.Listen(":3000")
 }
