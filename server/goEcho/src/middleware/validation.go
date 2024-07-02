ef240958c9f5417112a956e1bf33ffeee881953d
QUDUSKUNLE <quduskunle@gmail.com>
2024-07-02 14:38:32 +0100

    Register validation middleware

---
+++ server/goEcho/src/middleware/validation.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"net/http"
+	// "github.com/labstack/echo/v4/middleware"
+  "gopkg.in/go-playground/validator.v9"
+	"github.com/labstack/echo/v4"
+)
+
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(inter interface{}) error {
+	if err := cv.validator.Struct(inter); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
+
+func RegisterValidation(e *echo.Echo) *echo.Echo {
+	e.Validator = &CustomValidator{validator: validator.New()}
+	return e
+}
