47cd74921bb52d7bc8d24cb2270b0ee093682557
Abdul-Quddus Yekeen <quduskunle@gmail.com>
2024-07-07 17:15:36 +0100

    Update picked up (#40)
    * Updated pick up
    
    * update

--- design-patterns/facade/shipping/src/database/repository/pickup_repository.go
+++ design-patterns/facade/shipping/src/database/repository/pickup_repository.go
@@ -16,7 +16,6 @@ func (database *Database) QueryCreatePickUp(pickUp model.PickUp) error {
 }
 
 func (database *Database) QueryUpdatePickUp(pickUp model.PickUp) error {
-	fmt.Println(pickUp, "&&&&&&&&&")
 	query := `UPDATE pickups SET pick_up_at=$3, updated_at=$4, status=$5 WHERE id=$1 AND shipping_id=$2 AND carrier_id=$6`
 	_, err := database.Exec(query, pickUp.ID, pickUp.ShippingID, pickUp.PickUpAt, pickUp.UpdatedAt, pickUp.Status, pickUp.CarrierID)
 	if err != nil {
