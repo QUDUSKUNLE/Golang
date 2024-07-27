e574723bb9f20695042ae3adac62dc9de4d340e5
QUDUSKUNLE <quduskunle@gmail.com>
2024-07-27 20:20:33 +0100

    change where in query

--- design-patterns/facade/shipping/internal/adapters/repository/shipping_repository.go
+++ design-patterns/facade/shipping/internal/adapters/repository/shipping_repository.go
@@ -23,7 +23,7 @@ func (database *PostgresRepository) CreateShippingAdaptor(shipping domain.Shippi
 
 func (database *PostgresRepository) GetShippingsAdaptor(userID uuid.UUID, status string) ([]domain.Shipping, error) {
 	var shippings []domain.Shipping
-	result := database.db.Where(&domain.Shipping{UserID: userID}).Preload("Carrier").Limit(10).Find(&shippings, domain.Shipping{UserID: userID})
+	result := database.db.Preload("Carrier").Find(&shippings, domain.Shipping{UserID: userID}).Limit(10)
 	if result.Error != nil {
 		return []domain.Shipping{}, result.Error
 	}
