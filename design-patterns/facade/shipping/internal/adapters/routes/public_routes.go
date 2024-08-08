6b268c50b00956975278af585b63f48d4874098b
QUDUSKUNLE <quduskunle@gmail.com>
2024-08-08 02:50:01 +0100

    Rename e to public

--- design-patterns/facade/shipping/internal/adapters/routes/public_routes.go
+++ design-patterns/facade/shipping/internal/adapters/routes/public_routes.go
@@ -5,11 +5,11 @@ import (
 	"github.com/QUDUSKUNLE/shipping/internal/adapters/handlers"
 )
 
-func PublicRoutesAdaptor(e *echo.Group, handler *handlers.HTTPHandler) *echo.Group {
-	e.POST("/delivery", handler.DeliveryProduct)
-	e.POST("/reject", handler.RejectProduct)
-	e.POST("/users", handler.Register)
-	e.POST("/login", handler.Login)
-	e.POST("/resetPassword", handler.ResetPassword)
-	return e
+func PublicRoutesAdaptor(public *echo.Group, handler *handlers.HTTPHandler) *echo.Group {
+	public.POST("/delivery", handler.DeliveryProduct)
+	public.POST("/reject", handler.RejectProduct)
+	public.POST("/users", handler.Register)
+	public.POST("/login", handler.Login)
+	public.POST("/resetPassword", handler.ResetPassword)
+	return public
 }
