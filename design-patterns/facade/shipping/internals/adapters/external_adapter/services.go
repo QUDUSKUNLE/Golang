7670814b01d8b5564134a88a1053de56452ab74a
QUDUSKUNLE <quduskunle@gmail.com>
2024-09-12 12:25:35 +0100

    change externalHandler to terminal

--- design-patterns/facade/shipping/internals/adapters/external_adapter/services.go
+++ design-patterns/facade/shipping/internals/adapters/external_adapter/services.go
@@ -5,55 +5,55 @@ import (
 	"github.com/QUDUSKUNLE/shipping/internals/core/ports"
 )
 
-func (externalHandler *ExternalServicesHandler) TerminalCreatePackagingAdaptor(packaging domain.SingleTerminalPackagingDto) (map[string]interface{}, error ){
+func (terminal *ExternalServicesHandler) TerminalCreatePackagingAdaptor(packaging domain.SingleTerminalPackagingDto) (map[string]interface{}, error ){
 	var result map[string]interface{}
 	serviceHandler := NewExternalServicesFacade()
 	buildPackaging := serviceHandler.terminalService.BuildNewTerminalPackaging(packaging)
-	result, err := externalHandler.external.TerminalRequest(ports.PACKAGING, ports.POST, buildPackaging)
+	result, err := terminal.external.TerminalRequest(ports.PACKAGING, ports.POST, buildPackaging)
 	if err != nil {
 		return result, err
 	}
 	return result, nil
 }
 
-func (externalHandler *ExternalServicesHandler) TerminalCreateAddressAdaptor(address domain.Address) (map[string]interface{}, error ){
+func (terminal *ExternalServicesHandler) TerminalCreateAddressAdaptor(address domain.Address) (map[string]interface{}, error ){
 	var result map[string]interface{}
 	serviceHandler := NewExternalServicesFacade()
 	buildAddress := serviceHandler.terminalService.BuildNewTerminalAddress(address)
-	result, err := externalHandler.external.TerminalRequest(ports.ADDRESSES, ports.POST, buildAddress)
+	result, err := terminal.external.TerminalRequest(ports.ADDRESSES, ports.POST, buildAddress)
 	if err != nil {
 		return result, err
 	}
 	return result, nil
 }
 
-func (externalHandler *ExternalServicesHandler) TerminalGetRatesAdaptor(query domain.TerminalRatesQueryDto) (map[string]interface{}, error ){
+func (terminal *ExternalServicesHandler) TerminalGetRatesAdaptor(query domain.TerminalRatesQueryDto) (map[string]interface{}, error ){
 	var result map[string]interface{}
 	serviceHandler := NewExternalServicesFacade()
 	buildQuery := serviceHandler.terminalService.BuildNewTerminalRatesQuery(query)
-	result, err := externalHandler.external.TerminalRequest(ports.SHIPMENT, ports.GET, buildQuery)
+	result, err := terminal.external.TerminalRequest(ports.SHIPMENT, ports.GET, buildQuery)
 	if err != nil {
 		return result, err
 	}
 	return result, nil
 }
 
-func (externalHandler *ExternalServicesHandler) TerminalCreateParcelAdaptor(parcel domain.SingleTerminalParcelDto) (map[string]interface{}, error ){
+func (terminal *ExternalServicesHandler) TerminalCreateParcelAdaptor(parcel domain.SingleTerminalParcelDto) (map[string]interface{}, error ){
 	var result map[string]interface{}
 	serviceHandler := NewExternalServicesFacade()
 	builtParcel := serviceHandler.terminalService.BuildNewTerminalParcel(parcel)
-	result, err := externalHandler.external.TerminalRequest(ports.PARCELS, ports.POST, builtParcel)
+	result, err := terminal.external.TerminalRequest(ports.PARCELS, ports.POST, builtParcel)
 	if err != nil {
 		return result, err
 	}
 	return result, nil
 }
 
-func (externalHandler *ExternalServicesHandler) TerminalCreateShipmentAdaptor(shipment domain.SingleTerminalShipmentDto) (map[string]interface{}, error ){
+func (terminal *ExternalServicesHandler) TerminalCreateShipmentAdaptor(shipment domain.SingleTerminalShipmentDto) (map[string]interface{}, error ){
 	var result map[string]interface{}
 	serviceHandler := NewExternalServicesFacade()
 	builtShipment := serviceHandler.terminalService.BuildNewTerminalShipment(shipment)
-	result, err := externalHandler.external.TerminalRequest(ports.SHIPMENTS, ports.POST, builtShipment)
+	result, err := terminal.external.TerminalRequest(ports.SHIPMENTS, ports.POST, builtShipment)
 	if err != nil {
 		return result, err
 	}
