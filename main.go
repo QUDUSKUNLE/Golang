d58131d0caa3fb4e0f9127b2ab3e7de013092ef2
Abdul-Quddus Yekeen <quduskunle@gmail.com>
2024-10-01 15:34:18 +0100

    Add Variadic function (#50)

--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/QUDUSKUNLE/Golang/tutorial/slice"
-	"maps"
-	"strings"
+	// "fmt"
+	"github.com/QUDUSKUNLE/Golang/tutorial/variadic"
+	// "strings"
 )
 
 func main() {
-	fmt.Println("uninit:", slice.S, slice.S == nil, len(slice.S) == 0)
-
-	m := make(map[string]int)
-	m["k1"] = 7; m["k2"] = 13
-	fmt.Printf("map: %v\ntype: %T\n", m, m)
-
-	v1, v3 := m["k1"], m["k3"];
-	fmt.Println(v1, v3)
-	fmt.Println("len:", len(m))
-	clear(m)
-	fmt.Println("len:", len(m))
-
-	n := map[string]int{"foo": 1, "bar": 2}
-	n2 := map[string]int{"foo": 1, "bar": 2, "car": 3}
-
-	if maps.Equal(n, n2) {
-		fmt.Println("n == n2")
-	}
-	fmt.Println(strings.EqualFold("Boluwatife08971@", "BOLUWATIFE08971@"))
+	variadic.Sum(1, 2)
+	variadic.Sum(1, 2, 3)
+	nums := []int{1, 2, 3, 4}
+	variadic.Sum(nums...)
 }
