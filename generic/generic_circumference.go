3ad4fe02f626ae976d46b69f25dd51b8f538fc42
QUDUSKUNLE <quduskunle@gmail.com>
2026-06-02 16:36:00 +0100

    Worked on liveLock

--- generic/generic_circumference.go
+++ generic/generic_circumference.go
@@ -4,17 +4,16 @@ import "fmt"
 
 
 // Paramatized type in golang
-
 type Radius interface {
-	int64 | int8 | float64
+	int64 | int8 | float64 | int |float32
 }
 
-func Generic_Circumference[r int | float32](radius r) {
+func Generic_Circumference[r Radius](radius r) {
 	c := 2 * 3 * radius
-	fmt.Printf("The circumference is %v", c)
+	fmt.Printf("The circumference is %v.", c)
 }
 
 func Paramatized_Circumference[R Radius](radius R) {
 	c := 2 * 3 * radius
-	fmt.Printf("The circumference is %v", c)
+	fmt.Printf("The circumference is %v.", c)
 }
--- main.go
+++ main.go
@@ -1,51 +1,71 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
-	// "time"
-	// algo "github.com/QUDUSKUNLE/Golang/tutorial/Algorithm"
+	// "github.com/QUDUSKUNLE/Golang/tutorial/generic"
 )
 
-type value struct {
-	mu    sync.Mutex
-	value int
-}
 
 func main() {
-	// var memoryAccess sync.Mutex
-	// var data int
-	// go func() {
-	// 	memoryAccess.Lock()
-	// 	data++
-	// 	memoryAccess.Unlock()
-	// }()
-	// memoryAccess.Lock()
-	// if data == 0 {
-	// 	fmt.Printf("the value is, 0")
-	// } else {
-	// 	fmt.Printf("the value is %v.\n", data)
-	// }
-	// memoryAccess.Unlock()
-	// fmt.Println(algo.ExpressionMatter(1, 1, 1))
+	cadence := sync.NewCond(&sync.Mutex{})
 
-	var wg sync.WaitGroup
-	printSum := func(v1, v2 *value) {
-		defer wg.Done()
-		v1.mu.Lock()
-		defer v1.mu.Unlock()
+	go func() {
+		for range time.Tick(1 *time.Millisecond) {
+			cadence.Broadcast()
+		}
+	}()
 
-		time.Sleep(2 * time.Second)
-		v2.mu.Lock()
-		defer v2.mu.Unlock()
-
-		fmt.Printf("sum=%v\n", v1.value+v2.value)
+	takeStep := func() {
+		cadence.L.Lock()
+		cadence.Wait()
+		cadence.L.Unlock()
 	}
 
-	var a, b value
-	wg.Add(2)
-	go printSum(&a, &b)
-	go printSum(&b, &a)
-	wg.Wait()
+	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool {
+		fmt.Fprintf(out, " %v", dirName)
+		atomic.AddInt32(dir, 1)
+		takeStep()
+		if atomic.LoadInt32(dir) == 1 {
+			fmt.Fprint(out, ". Success!")
+			return true
+		}
+		takeStep()
+		atomic.AddInt32(dir, -1)
+		return false
+	}
+
+	var left, right int32
+	tryLeft := func (out *bytes.Buffer) bool {
+		return tryDir("left", &left, out)
+	}
+
+	tryRight := func (out *bytes.Buffer) bool {
+		return tryDir("right", &right, out)
+	}
+
+	walk := func(walking *sync.WaitGroup, name string) {
+		var out bytes.Buffer
+		defer func() {
+			fmt.Println(out.String())
+		}()
+		defer walking.Done()
+
+		fmt.Fprintf(&out, "%v is trying to scoot:", name)
+		for i := 0; i < 5; i++ {
+			if tryLeft(&out) || tryRight(&out) {
+				return
+			}
+		}
+		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperation!", name)
+	}
+	var peopleInHallway sync.WaitGroup
+	peopleInHallway.Add(2)
+	go walk(&peopleInHallway, "Alice")
+	go walk(&peopleInHallway, "Barbara")
+	peopleInHallway.Wait()
+	// generic.Paramatized_Circumference(12.67)
 }
